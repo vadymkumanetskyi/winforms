c030fc1e64d4e272ac3266833e39aa1c366103f0
vadymkumanetskyi <vadimkumanecij@gmail.com>
2026-04-13 10:14:48 +0300

    asdasd

--- WinFormsApp6/Form1.cs
+++ WinFormsApp6/Form1.cs
@@ -2,7 +2,7 @@ namespace WinFormsApp6
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(TextBox myTextBox)
         {
             InitializeComponent();
             Button btn = new Button();
@@ -11,10 +11,15 @@ namespace WinFormsApp6
             btn.Height = 40;
             btn.Left = 50;
             btn.Top = 30;
-            btn.Controls.Add(btn);
+            this.Controls.Add(btn);
             btn.Click += Btn_Click;
-            TextBox textBox = new TextBox();
+            myTextBox.Location = new Point(250, 100);
+            myTextBox.Width = 100;
+            myTextBox.Height = 100;
+            myTextBox.Text = "hello";
+            this.Controls.Add(myTextBox);
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             
@@ -23,6 +28,8 @@ namespace WinFormsApp6
         {
             this.BackColor = Color.Red;
         }
+
+
 
         
     }
